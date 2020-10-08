02c278e6d04604e5b0095aacca4fc63bbfbd2486
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-08 15:40:28 +0000

    Asked the user for 10 integers and then asked a random one to locate

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program that determines where a user entered number is out of 10 numbers
+ * @author Daniel Rahmani
  */
 public class Main {
 
@@ -9,7 +10,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    int[] integers = new int[10];
+
+    // ask the user to enter 10 integers
+    System.out.println("Please enter in 10 integers to put into the array");
+    // create a for loop to get the 10 integers
+    for(int i = 0; i < 10; i++){
+    integers[i] = input.nextInt();
+    }
+
+    // ask the user for a number to find
+    System.out.println("Please enter a number to find");
+    int findNum = input.nextInt();
+
+
+    // create a for loop to find the matching numbers
+    for(int i = 0; i < 10; i++){
+    // create an if statement to locate where the numbers are 
+    if(findNum == integers[i]){
+      System.out.println(findNum + " is located at index " + i);
+    }
+    }
   }
 }
